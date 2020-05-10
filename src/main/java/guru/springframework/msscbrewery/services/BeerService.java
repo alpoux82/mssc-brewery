c1df8fde831d4bf045d0a6eb927481ef241022e2
Alberto <alberto.poyatos.uxo@gmail.com>
2020-05-10 19:18:05 +0200

    Fix @RequestBody

--- src/main/java/guru/springframework/msscbrewery/services/BeerService.java
+++ src/main/java/guru/springframework/msscbrewery/services/BeerService.java
@@ -6,4 +6,8 @@ import java.util.UUID;
 
 public interface BeerService {
     BeerDto getBeerById(UUID beerId);
+
+    BeerDto save(BeerDto beerDto);
+
+    void update(UUID beerId, BeerDto beerDto);
 }
--- src/main/java/guru/springframework/msscbrewery/services/BeerServiceImpl.java
+++ src/main/java/guru/springframework/msscbrewery/services/BeerServiceImpl.java
@@ -14,4 +14,17 @@ public class BeerServiceImpl implements BeerService {
                 .beerStyle("Pale Ale")
                 .build();
     }
+
+    @Override
+    public BeerDto save(BeerDto beerDto) {
+
+        return BeerDto.builder()
+                .id(UUID.randomUUID())
+                .build();
+    }
+
+    @Override
+    public void update(UUID beerId, BeerDto beerDto) {
+
+    }
 }
